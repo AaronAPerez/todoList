664eb3db3a5442fb022ab97c446b9ac28756e3c5
AaronAPerez <aaperez06@gmail.com>
2024-05-20 23:19:06 -0700

    Added Style, Edit, and Delete buttons for each list item

--- todoList/src/Components/Todo/Todo.tsx
+++ todoList/src/Components/Todo/Todo.tsx
@@ -1,44 +1,121 @@
 import { Key, useState } from "react";
+import { FcPlus } from "react-icons/fc";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { TfiPencilAlt } from "react-icons/tfi";
 interface TodoItem {
-  id: Key | null | undefined;
+  id: Key;
   todo: string;
+  isCompleted: boolean;
 }
 
 
 const Todo = () => {
   const [input, setInput] = useState("");
-
   const [list, setList] = useState<TodoItem[]>([]);
+  const [editingId, setEditingId] = useState<Key>("");
+  const [editingText, setEditingText] = useState("");
 
   const addTodo = (newItem: string) => {
     const newTodo: TodoItem = {
       id: Math.random(),
       todo: newItem,
+      isCompleted: false,
     };
+
     setList([...list,newTodo]);
     setInput("");
   };
+
+// Function to delete a todo item
+const deleteTodo = (id: Key) => {
+  const updatedList = list.filter((item) => item.id !== id);
+setList(updatedList);
+};
+
+
+// Function to toggle the completion state of a todo item
+const toggleComplete = (id: Key) => {
+  const updatedList = list.map((item) =>
+    item.id === id ? { ...item, isCompleted: !item.isCompleted } : item
+  );
+  setList(updatedList);
+};
+
+
+const startEditing = (id: Key, text: string) => {
+  setEditingId(id);
+  setEditingText(text);
+};
+
+const stopEditing = () => {
+  setEditingId("");
+  setEditingText("");
+};
+
+const updateTodo = (id: Key) => {
+  const updatedList = list.map((item) =>
+  item.id === id ? { ...item, todo: editingText } : item
+);
+  
+// Update the list state with the newly created list containing the updated todo text
+setList(updatedList);
+stopEditing();
+};
 
   return (
     <>
       <div className="myContainer">
         <div className="row">
+          <div className="col-1"></div>
           <h1>Todo List</h1>
           <input
             type="text"
+            placeholder="Enter a task...."
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
           <button className="addButton" onClick={() => addTodo(input)}>
-            Add
+          <FcPlus />  Add
           </button>
         </div>
-
         <ul>
+          
           {list.map((item) => (
-            <li key={item.id}>
-              {item.todo}
-              <button>X</button>
+            <li key={item.id}
+            style={{ textDecoration: item.isCompleted ? "line-through" : "none" }}>     
+            <input
+            type="checkbox"
+            checked={item.isCompleted}
+            onChange={() => toggleComplete(item.id)}
+          />
+          {editingId === item.id ? (
+                <input
+                  type="text"
+                  value={editingText}
+                  onChange={(e) => setEditingText(e.target.value)}
+                  onBlur={() => stopEditing()}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      updateTodo(item.id);
+                    } else if (e.key === "Escape") {
+                      stopEditing();
+                    }
+                  }}
+                />
+              ) : (
+                item.todo
+              )}
+              <button onClick={() => deleteTodo(item.id)}>
+                <FaRegTrashCan color="red" size={20} />
+                </button>
+              {/* <input
+                type="checkbox"
+                checked={item.isCompleted}
+                onChange={() => toggleComplete(item.id)}
+              /> */}
+              <button onClick={() => startEditing(item.id, item.todo)}>
+              <TfiPencilAlt color="blue" size={20}/>
+              </button>
             </li>
           ))}
         </ul>
@@ -48,3 +125,4 @@ const Todo = () => {
 };
 
 export default Todo;
+
